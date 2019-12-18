cc8a2582879959c1e3d83794dfbd6f987e55d901
khushboo hathiwala <khushboo.hathiwala@gmail.com>
2019-12-18 17:36:32 +0530

    report service admin email updates

--- src/main/java/com/beautifulyears/constants/BYConstants.java
+++ src/main/java/com/beautifulyears/constants/BYConstants.java
@@ -7,7 +7,7 @@ public final class BYConstants {
 	// public static final String IMAGE_CDN_PATH = "/home/ubuntu/uploads"; //for
 	// remote
 	public static final String IMAGE_CDN_PATH = "c:/uploads"; // for local
-	public static final List<String> ADMIN_EMAILS = Arrays.asList("nitin.j@beautifulyears.com");
+	public static final List<String> ADMIN_EMAILS = Arrays.asList("pavithra@socialalpah.org");
 
 	public static final String SOCIAL_SIGNON_PLATFORM_GOOGLE = "google"; 
 
