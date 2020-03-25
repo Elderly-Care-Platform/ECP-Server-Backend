6b49fe6be720f23d8188f7a267703af45f69b778
khushboo hathiwala <khushboo.hathiwala@gmail.com>
2020-03-25 20:05:36 +0530

    created auto suggestion api for services

--- src/main/java/com/beautifulyears/justdial/JustDialHandler.java
+++ src/main/java/com/beautifulyears/justdial/JustDialHandler.java
@@ -28,6 +28,7 @@ public class JustDialHandler {
     private String baseUrl = "http://win.justdial.com/tata-v1";
     private String createToken = "/createToken.php";
     private String justDialsearch = "/searchziva.php?";
+    private String autoSuggest = "/autosuggest.php?";
     private String categories = "/catid_list.php";
     private String city = "Hyderabad";
 
@@ -348,6 +349,54 @@ public class JustDialHandler {
             throw new RuntimeException("ERROR in search from Justdial. " + e);
         }
         return result;
+
+    }
+
+    public JSONArray getAutosuggest(String search) {
+        LoggerUtil.logEntry();
+        String response = null;
+        JSONArray results = null;
+        try {
+            String postUrl = this.baseUrl + this.autoSuggest;
+
+            // Prepare parameter string
+            StringBuilder sbPostData = new StringBuilder(postUrl);
+            sbPostData.append("&search=" + URLEncoder.encode(search, "UTF-8"));
+
+            // final string
+            postUrl = sbPostData.toString();
+
+            logger.debug(postUrl);
+
+            URL u = new URL(postUrl);
+            String type = "application/json";
+            String encodedData = sbPostData.toString();
+            HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", type);
+            // conn.setRequestProperty("Content-Length",
+            // String.valueOf(encodedData.length()));
+            OutputStream os = conn.getOutputStream();
+            os.write(encodedData.getBytes());
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer b = new StringBuffer();
+            while ((inputLine = in.readLine()) != null)
+                b.append(inputLine + "\n");
+            in.close();
+            response = b.toString();
+            JSONObject json = new JSONObject(response);
+            results = json.getJSONArray("results");
+
+            // result = resultsObject.toString();
+
+            logger.debug(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // throw new RuntimeException("ERROR in search from Justdial. " + e);
+        }
+        return results;
 
     }
 
